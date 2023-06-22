0345b5f03169ea57db9a941981bad135eb374b0f
almat2007@inbox.ru <almat2007@inbox.ru>
2023-06-22 03:59:28 +0000

    last

--- models/test.sql
+++ models/test.sql
@@ -1,14 +1,20 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+
 with FBIMP as (
     select sum(a.impressions) as impressions,'FB' as channel,sum(a.spend)/sum(a.clicks) as CPC 
-    from {{ ref('src_ads_creative_facebook_all_data') }} a
+    from {{ ref('stg_src_ads_creative_facebook_all_data') }} a
     ),
 TKIMPT as (
     select sum(b.impressions) as impressions,'TK' as channel,sum(b.spend)/sum(b.clicks) as CPC
-    from {{ ref('src_ads_tiktok_ads_all_data') }} b
+    from {{ ref('stg_src_ads_tiktok_ads_all_data') }} b
     ),
 TWIMPT as (
     select sum(c.impressions) as impressions,'TW' as channel,sum(c.spend)/sum(c.clicks) as CPC 
-    from {{ ref('src_promoted_tweets_twitter_all_data') }} c )
+    from {{ ref('stg_src_promoted_tweets_twitter_all_data') }} c )
 select * from FBIMP
 union all 
 select * from TKIMPT
--- models/test1.sql
+++ models/test1.sql
@@ -1,5 +1,11 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+
 select 
 sum(a.spend)/sum(a.clicks) as CPC, 
 sum(a.spend)/sum(a.conv) as Conversion_cost, 
 sum(a.spend)/sum(a.clicks) as Cost_per_engage, 
-from {{ref("src_ads_bing_all_data")}} a
+from {{ref("stg_src_ads_bing_all_data")}} a
